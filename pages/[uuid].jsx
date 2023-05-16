a52e7ce936354d617b046f416c0b3010bc43fcfb
Muhammed rinshad s r <81734540+DareDevilStudios@users.noreply.github.com>
2023-05-16 15:32:58 +0530

    images length are correted in all screens

--- pages/[uuid].jsx
+++ pages/[uuid].jsx
@@ -60,9 +60,9 @@ export default function Home({ params }) {
                 console.error(error);
             });
 
-            setTimeout(() => {
-                setLoading(!loading)
-            }, 3000);
+        setTimeout(() => {
+            setLoading(!loading)
+        }, 3000);
     }, [])
 
     // useEffect(() => {
@@ -99,7 +99,8 @@ export default function Home({ params }) {
             </Head>
 
             {
-                loading ? (
+                loading 
+                ? (
                     <div class="w-full h-screen justify-center items-center animate-pulse flex flex-1 bg-[#f5f7fb]">
                         <Image src={Logo} className="w-[40rem]" alt="Raasees Properties" />
                     </div>
@@ -132,24 +133,21 @@ export default function Home({ params }) {
                                                     img1.map((image, index) => (
                                                         <div key={image.uuid}>
                                                             <Image
-                                                                className=""
+                                                                className="w-[319px] h-[179px]"
                                                                 src={image.image_url_thumbnail_1080}
                                                                 alt="apartments"
                                                                 width={800}
-                                                                height={800}
+                                                                height={300}
                                                             />
                                                             {/* 319 × 179 px */}
                                                         </div>
                                                     ))}
                                                 {/* {dummy.map((image) => (
-                    <div class="flex items-center justify-center w-full h-[179px] bg-gray-300 rounded animate-pulse">
-                      <svg class="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
-                    </div>
-                  ))} */}
+                                                    <div class="flex items-center justify-center w-full h-[179px] bg-gray-300 rounded animate-pulse">
+                                                    <svg class="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
+                                                    </div>
+                                                ))} */}
                                                 <div onClick={() => setShowModal(!showModal)} class="flex flex-col items-center justify-center w-full h-full bg-gray-300 rounded animate-pulse">
-                                                    {/* <svg className='w-12 h-12' fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"></path>
-                    </svg> */}
                                                     <span className='text-2xl text-black'>More Images</span>
                                                 </div>
                                             </div>
@@ -164,23 +162,24 @@ export default function Home({ params }) {
                                         <div class="flex gap-4 items-center mt-4">
                                             <span class="text-gray-800 text-lg font-semibold">{property.project}</span>
                                             {/* <div class="flex items-center gap-1">
-                  <svg class="w-8 h-8 text-transparent" fill="#fbcb50" stroke="currentColor"
-                    stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
-                    </path>
-                  </svg>
-                  <span class="text-gray-800 text-lg font-md">{property.avg_rating}</span>
-                  <span class="text-gray-500 text-lg">({property.rating_count})</span>
-                </div> */}
-                                            {/* <svg class="w-10 h-10 text-gray-500 border-2 rounded-full p-1" fill="grey" stroke="currentColor"
-                  stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z">
-                  </path>
-                </svg> */}
+                                                <svg class="w-8 h-8 text-transparent" fill="#fbcb50" stroke="currentColor"
+                                                    stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                                    aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
+                                                    </path>
+                                                </svg>
+                                                <span class="text-gray-800 text-lg font-md">{property.avg_rating}</span>
+                                                <span class="text-gray-500 text-lg">({property.rating_count})</span>
+                                                </div> */}
+                                                                            {/* <svg class="w-10 h-10 text-gray-500 border-2 rounded-full p-1" fill="grey" stroke="currentColor"
+                                                stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                                aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z">
+                                                </path>
+                                                </svg> 
+                                            */}
                                         </div>
 
                                         {/* <!-- address --> */}
@@ -365,7 +364,7 @@ export default function Home({ params }) {
                                                         <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
                                                             {
                                                                 property.property_images.map((image, index) => (
-                                                                    <Image class="h-auto max-w-full rounded-lg" src={image.image_url_thumbnail_1080} width={300} height={300} alt="" />
+                                                                    <Image class="w-[319px] h-[179px] rounded-lg" src={image.image_url_thumbnail_1080} width={300} height={300} alt="" />
                                                                 ))}
                                                         </div>
 
